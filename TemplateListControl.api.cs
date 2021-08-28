8b9c04f43a76cdd6c85f7d3e8a210d766fecd46b
Sergey Stoyan <sergey.stoyan@gmail.com>
2021-08-28 11:42:10 +0300

    .

--- TemplateListControl.api.cs
+++ TemplateListControl.api.cs
@@ -1,6 +1,7 @@
 ﻿//********************************************************************************************
 //Author: Sergey Stoyan
 //        sergey.stoyan@gmail.com
+//        sergey.stoyan@hotmail.com
 //        http://www.cliversoft.com
 //********************************************************************************************
 using System;
@@ -313,42 +314,5 @@ namespace Cliver.ParserTemplateList
 
         public event Func<bool> SavingFromGui;
         public event Action Loading2Gui;
-
-        public class FormManager
-        {
-            static Dictionary<Type, Dictionary<DataGridViewRow, Form>> formTypes2rows2form = new Dictionary<Type, Dictionary<DataGridViewRow, Form>>();
-
-            static public T Get<T>(DataGridViewRow row) where T : Form
-            {
-                Dictionary<DataGridViewRow, Form> rows2form = getRows2form(typeof(T));
-                rows2form.TryGetValue(row, out Form form);
-                if (form?.IsDisposed == true)
-                {
-                    rows2form.Remove(row);
-                    form = null;
-                }
-                return (T)form;
-            }
-
-            static Dictionary<DataGridViewRow, Form> getRows2form(Type formType)
-            {
-                if (!formTypes2rows2form.TryGetValue(formType, out Dictionary<DataGridViewRow, Form> rows2form))
-                {
-                    rows2form = new Dictionary<DataGridViewRow, Form>();
-                    formTypes2rows2form[formType] = rows2form;
-                }
-                return rows2form;
-            }
-
-            static public void Set(DataGridViewRow row, Form form)
-            {
-                Dictionary<DataGridViewRow, Form> rows2form = getRows2form(form.GetType());
-                form.FormClosed += delegate
-                {
-                    rows2form.Remove(row);
-                };
-                rows2form[row] = form;
-            }
-        }
     }
 }
